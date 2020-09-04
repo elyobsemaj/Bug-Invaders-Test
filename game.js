8ecc1be5f4086128dd4a8c0985b324bac55b6d68
elyobsemaj <54197717+elyobsemaj@users.noreply.github.com>
2020-09-04 16:04:20 -0700

    Update game.js

--- game.js
+++ game.js
@@ -34,6 +34,11 @@ function create() {
 	})
 
 	gameState.enemies = this.physics.add.group();
+	  for (yVal = 1; yVal < 4; yVal++) {
+    	for (xVal = 1; xVal < 9; xVal++) {
+      gameState.enemies.create(50 * xVal, 50 * yVal, 'bug1').setScale(0.6).setGravityY(-200);
+    		}
+  	}
 
 	
 	
