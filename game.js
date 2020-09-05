270d365574ffff1b4f3690e6226dda23d9351fea
elyobsemaj <54197717+elyobsemaj@users.noreply.github.com>
2020-09-04 21:34:10 -0700

    Update game.js

--- game.js
+++ game.js
@@ -3,6 +3,11 @@ function preload() {
   this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_2.png');
   this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_3.png');
   this.load.image('bugPellet', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bugPellet.png');
+  this.load.image('tower1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_1.png');
+  this.load.image('tower2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_2.png');
+  this.load.image('tower3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_3.png');
+  this.load.image('tower4', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_1.png');
+  this.load.image('tower5', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_2.png');
 }
 
 
@@ -14,14 +19,43 @@ function create() {
 	    path = this.add.path(config.width / 16, config.height * 1.1);
 	    path.lineTo(config.width / 16, config.height * .55);
 	    path.lineTo(config.width * 1.1, config.height * .55);
-    
 	    graphics.lineStyle(3, 0xffffff, 1);
 	    // visualize the path
 	    path.draw(graphics);
 	
 	    // add enemies
-    	var enemy1 = this.add.follower(path, config.width / 16, config.height * 1.1, 'bug1');
-    	enemy1.startFollow(8000);
+            for (var i = 0; i < 8; i++)
+             {
+            var follower = this.add.follower(path, config.width / 16, config.height * 1.1, 'bug1');
+
+            follower.startFollow({
+                duration: 8000,
+                positionOnPath: true,
+                repeat: 0,
+                ease: 'Linear',
+                delay: i * 700
+            });
+             }
+        //add grid
+        var group = this.add.group({
+            key: 'bug2',
+            frameQuantity: 15
+        });
+        Phaser.Actions.GridAlign(group.getChildren(), {
+            width: 5,
+            height: 3,
+            cellWidth: 40,
+            cellHeight: 40,
+            x: config.width/3,
+            y:config.height * .66
+        });
+        // add towers
+        gameState.towers = this.physics.add.group();
+        for (xVal = 1; xVal < 6; xVal++) {
+            gameState.towers.create((config.width/5) + (xVal*40), config.height*.9, 'tower1');
+        }
+        gameState.towers.inputEnabled = true;
+        gameState.towers.input.enableDrag();
 }
 
 function update() {
@@ -35,7 +69,7 @@ const config = {
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 },
+			gravity: { y: 0 },
 			enableBody: true,
 		}
 	},
